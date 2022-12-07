c984b029cf313be33bfafb8f2af8b539667e73f0
matheuskaully <kaully.santos@gmail.com>
2022-12-07 11:38:35 -0300

    adiciona outros endereços

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,8 @@ import axios from 'axios'
 export const api = axios.create({
   baseURL: `${
     'https://dtmoney-matheuskaully.vercel.app/' ||
-    'https://dtmoney-rouge-mu.vercel.app/' ||
-    'https://dtmoney-cdtve3emg-matheuskaully.vercel.app/'
+    'https://dtmoney-liart.vercel.app/' ||
+    'https://dtmoney-git-main-matheuskaully.vercel.app/' ||
+    'https://dtmoney-mi9hxooxc-matheuskaully.vercel.app/'
   }api` 
 })
